28559a608dbb900bfc30dd9686c680a2f6626d69
hswgt <steffen-sonntag@web.de>
2011-04-23 14:08:27 +0200

    spass

---
+++ test/creeper.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(int argc, char ** argv) {
+	printf("\n");
+	printf("  %c%c  %c%c \n", 178, 178, 178, 178);
+	printf("  **  ** \n");
+	printf("    **   \n");
+	printf("   ****  \n");
+	printf("   ****  \n");
+	printf("   ****  \n");
+	printf("   *  *  \n");
+	printf("\n");
+}
